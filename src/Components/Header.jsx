989fd66aef26804188a9159512dff2059357aebb
kameshravada <ravadavenkatakamesh123@gmail.com>
2025-08-12 11:55:30 +0530

    4th commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="bg-[#0d1224] text-white px-6 py-5 border-b border-gray-600 ">
-      <img src="/src/assets/dllogo.png" alt="" className="w-52" />
+      <img src="/src/assets/dl_logo.png" alt="" className="w-52" />
     </div>
   );
 };
